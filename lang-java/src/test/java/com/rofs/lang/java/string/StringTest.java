295208a66a264940e6abfdeaed415e7e1bf6fc8a
river <riveroverflows705@gmail.com>
2025-01-21 10:21:10 +0900

    java 모듈 설정

--- lang-java/src/test/java/com/rofs/lang/java/string/StringTest.java
+++ lang-java/src/test/java/com/rofs/lang/java/string/StringTest.java
@@ -1,12 +1,8 @@
 package com.rofs.lang.java.string;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.StringUtils;
 
 public class StringTest {
 
@@ -19,31 +15,30 @@ public class StringTest {
         System.out.println(list2.containsAll(list1));
     }
 
+//    @Test
+//    void textBlockTest() {
+//        String textBlock = """
+//                           Hello, World!
+//                           This is a text block.
+//                           """;
+//        String normalText = "Hello, World!\nThis is a text block.\n";
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        try {
+//            String s = objectMapper.writeValueAsString(textBlock);
+//            System.out.println("s = " + s);
+//        } catch (JsonProcessingException ignore) {
+//        }
+//        System.out.println(textBlock);
+//        System.out.println(normalText);
+//    }
 
-    @Test
-    void textBlockTest() {
-        String textBlock = """
-                           Hello, World!
-                           This is a text block.
-                           """;
-        String normalText = "Hello, World!\nThis is a text block.\n";
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            String s = objectMapper.writeValueAsString(textBlock);
-            System.out.println("s = " + s);
-        } catch (JsonProcessingException ignore) {
-        }
-        System.out.println(textBlock);
-        System.out.println(normalText);
-    }
-
-    @Test
-    void splitTest() {
-        String str = "10000";
-        List<String> list = split(str, "\\.");
-        System.out.println(list);
-    }
+//    @Test
+//    void splitTest() {
+//        String str = "10000";
+//        List<String> list = split(str, "\\.");
+//        System.out.println(list);
+//    }
 
     private String removeWhitespaces(String str) {
         if (str == null) {
@@ -52,19 +47,18 @@ public class StringTest {
         return str.replaceAll("\\s+", "");
     }
 
-    private boolean hasText(String str) {
-        return StringUtils.hasText(removeWhitespaces(str));
-    }
+//    private boolean hasText(String str) {
+//        return StringUtils.hasText(removeWhitespaces(str));
+//    }
 
-
-    private List<String> split(String str, String separator) {
-        if (!hasText(str)) {
-            return List.of();
-        }
-        String defaultSeparator = ",";
-        String _separator = hasText(separator) ? separator : defaultSeparator;
-        return Arrays.asList(str.split(_separator));
-    }
+//    private List<String> split(String str, String separator) {
+//        if (!hasText(str)) {
+//            return List.of();
+//        }
+//        String defaultSeparator = ",";
+//        String _separator = hasText(separator) ? separator : defaultSeparator;
+//        return Arrays.asList(str.split(_separator));
+//    }
 
     @Test
     void tt() {
