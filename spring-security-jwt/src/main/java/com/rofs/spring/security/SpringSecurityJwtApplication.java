235ac349f222524ff121ce45356778c6d3cdb0f6
river <riveroverflows705@gmail.com>
2025-02-05 15:34:18 +0900

    sprint-security-jwt: secret key 생성 후 환경변수로 추가

--- spring-security-jwt/src/main/java/com/rofs/spring/security/SpringSecurityJwtApplication.java
+++ spring-security-jwt/src/main/java/com/rofs/spring/security/SpringSecurityJwtApplication.java
@@ -1,13 +1,24 @@
 package com.rofs.spring.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Encoders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.crypto.SecretKey;
 
 @SpringBootApplication
 public class SpringSecurityJwtApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityJwtApplication.class, args);
-	}
+    public static void main(String[] args) {
+//        generateSecretKey();
+        SpringApplication.run(SpringSecurityJwtApplication.class, args);
+    }
+
+    private static void generateSecretKey() {
+        SecretKey key = Jwts.SIG.HS256.key().build();
+        String secretKey = Encoders.BASE64.encode(key.getEncoded());
+        System.out.println("Generated Secret Key: " + secretKey);
+    }
 
 }
