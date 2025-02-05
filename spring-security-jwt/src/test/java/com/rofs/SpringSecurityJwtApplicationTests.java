5bd826e47c21f766084415fe57c4b4f152186daf
river <riveroverflows705@gmail.com>
2025-02-05 13:57:12 +0900

    sprint-security-jwt: 스프링 시큐리티 with jwt 모듈 생성

---
+++ spring-security-jwt/src/test/java/com/rofs/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rofs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
