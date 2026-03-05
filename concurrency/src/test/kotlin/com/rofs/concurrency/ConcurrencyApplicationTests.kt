c72c6165017f52a81277efdd0ae5ae2ffe0f59c2
river <bluuminn@gmail.com>
2026-03-06 04:09:08 +0900

    concurrency 모듈 추가

---
+++ concurrency/src/test/kotlin/com/rofs/concurrency/ConcurrencyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rofs.concurrency
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConcurrencyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
