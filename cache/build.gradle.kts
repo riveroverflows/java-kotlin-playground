7ad687620a23833bf48f37b4b6815f92006d72aa
river <bluuminn@gmail.com>
2026-03-13 03:23:46 +0900

    feat(cache): add cache module

---
+++ cache/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    java
+}
+
+group = "com.rofs"
+version = "0.0.1-SNAPSHOT"
+description = "cache"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
