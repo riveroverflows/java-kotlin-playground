55886b4730a90ca6cd83e17676023cfdcec6bf81
river <bluuminn@gmail.com>
2026-04-15 05:13:39 +0900

    feat: spring-boot-batch-5 모듈 추가

---
+++ spring-boot-batch-5/src/main/java/com/r11s/batch/SpringBootBatch5Application.java
@@ -0,0 +1,13 @@
+package com.r11s.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatch5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatch5Application.class, args);
+	}
+
+}
---
+++ spring-boot-batch-5/src/test/java/com/r11s/batch/SpringBootBatch5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r11s.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBatch5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
